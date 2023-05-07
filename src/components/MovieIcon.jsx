ba12ff9692cfc63c67405d607dbfd944c22bfcd5
Savion Watson <72318904+SavionW-Works@users.noreply.github.com>
2023-05-06 21:11:26 -0400

    Minor Visual Adjustments
    -Adjusted the way the movie icons work, so titles, genres and overlays are always centered
    
    -Shrank size of and adjusted text of overlays

--- src/components/MovieIcon.jsx
+++ src/components/MovieIcon.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import AddItem from "./AddItem";
 import { Container, Row, Col } from "react-bootstrap";
 const MovieIcon = (props) => {
   const ItemComponent = props.itemComponent;
   return (
     <>
       {props.movies.map((movie, index) => (
-        <Container>
+        <Container className="justify-content-center">
           <Col
-            className="image-container d-flex justify-content-start  m-3"
+            className="image-container d-flex justify-content-center  m-3"
             key={movie.imdbID}
           >
-            <img src={movie.Poster} alt="Movie Poster"></img>
+            <img
+              src={movie.Poster}
+              alt="Movie Poster"
+              className="justify-content-start"
+            />
             <div
               onClick={() => props.handleItemsClick(movie)}
               className="overlay d-flex align-items-center justify-content-center"
@@ -19,12 +22,15 @@ const MovieIcon = (props) => {
               <ItemComponent />
             </div>
           </Col>
-          <div className="text-center">
-            <h3 className="fw-bolder">
-              {movie.Title} ({movie.Year})
-            </h3>
-            <h4 className="fw-bolder text-warning">{movie.Genres}</h4>
-          </div>
+
+          <Col className="justify-content-start m-3 mt-5">
+            <div className="text-center">
+              <h3 className="fw-bolder">
+                {movie.Title} ({movie.Year})
+              </h3>
+              <h4 className="fw-bolder text-warning">{movie.Genres}</h4>
+            </div>
+          </Col>
         </Container>
       ))}
     </>
